55fa72ae2181473fb3e4d77d200822b73784469e
Ahsen Hira Mimtaş <xahsenhiraw@gmail.com>
2025-08-09 16:53:38 +0300

    addded formatted string

--- program.py
+++ program.py
@@ -1,5 +1,5 @@
 #Değişkenler - Variables 
 isim = input("Lütfen adınızı giriniz: ")
-print("Merhaba " + isim  +  "\tHoşgeldiniz!")
+print(f"Merhaba {isim} Hoşgeldiniz!") #fortmatted string kullanımı
 print("Sayın " + isim + " Aşağıdaki seçeneklerden birini seçiniz: ")
-      
+    
