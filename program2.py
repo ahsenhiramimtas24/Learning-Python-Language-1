c3264ddf7cfcc0902f4734599f88093e627d4ee3
Ahsen Hira Mimtaş <xahsenhiraw@gmail.com>
2025-08-15 15:18:45 +0300

    learning basic data types ( string, integer, float and boolean )

---
+++ program2.py
@@ -0,0 +1,23 @@
+# sting veri tipi
+ad = "Ahsen" 
+soyad = "Mimtaş"
+isim = ad + " " + soyad
+yeniad = ad * 2 # => ad + ad
+print(isim)
+print(yeniad)
+print(type(yeniad))
+
+# int ( integer) veri tipi
+sayi1 = 10
+print(sayi1 + 10) # toplama işlemi
+print(type(sayi1))
+
+# float veri tipi (kayan noktalı sayılar)
+print(10.3 + 10.2)
+print(type(10.3))
+
+# bool ( boolean ) veri tipi ( Mantıksal değerler tutulur - True/False )
+flag = True
+print(type(flag))
+print(10<20)
+print(10<3)
