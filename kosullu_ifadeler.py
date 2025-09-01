1428e1b49c8f89be52e9c8d07bbac5ad43fa7f4a
Ahsen Hira Mimtaş <xahsenhiraw@gmail.com>
2025-09-01 18:14:13 +0300

    added elif

--- kosullu_ifadeler.py
+++ kosullu_ifadeler.py
@@ -1,10 +1,21 @@
 #kullanıcıdan sıcaklığın değerini al (float)
 sicaklik = float(input("Lütfen sıcaklık değerini giriniz: "))
 
-# gerekli sıcaklık bilgisini al ve kontrol edip gerekli mesajı yazdır
+#gerekli sıcaklık bilgisini al ve kontrol edip gerekli mesajı yazdır
 if sicaklik > 37:
     print("Ateş değeriniz yüksek!.")
     print("Lütfen doktorunuza başvurunuz.")
 else: 
     print("Ateş değeriniz normal.")
     print("Geçmiş olsun.")
+
+
+    sayi = 8
+if  sayi > 0:
+    print("Sayı pozitiftir.")
+    
+elif sayi < 0:
+    print("Sayı negatiftir.")
+
+else: 
+    print("Sayı sıfırdır.")
