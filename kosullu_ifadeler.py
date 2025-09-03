69bb9e321570cae6312749ccf7ab37b5064ec56a
Ahsen Hira Mimtaş <xahsenhiraw@gmail.com>
2025-09-03 17:29:55 +0300

    added more details

--- kosullu_ifadeler.py
+++ kosullu_ifadeler.py
@@ -1,7 +1,9 @@
 #kullanıcıdan sıcaklığın değerini al (float)
+"""
 sicaklik = float(input("Lütfen sıcaklık değerini giriniz: "))
 
 #gerekli sıcaklık bilgisini al ve kontrol edip gerekli mesajı yazdır
+
 if sicaklik > 37:
     print("Ateş değeriniz yüksek!.")
     print("Lütfen doktorunuza başvurunuz.")
@@ -17,3 +19,19 @@ elif sayi < 0:
     print("Sayı negatiftir.")
 else: 
     print("Sayı sıfırdır.")
+   """
+
+#daha dikkatli sıcaklık ölçümü
+
+sicaklik = float(input("Lütfen sıcaklık değerini giriniz: "))
+
+if sicaklik < 36.1:
+    print( " Sıcaklık değeri düşük, hipotermi riski var.")
+elif sicaklik <= 37.2:
+    print("Sıcaklık değeri normal.")
+elif sicaklik <= 38.0:
+    print("Sıcaklık değeri hafif yüksek, dikkatli olun.")
+elif sicaklik <= 39.0:
+    print("Sıcaklık değeri yüksek, dinlenin ve bol sıvı alın.")
+else:
+    print("Sıcaklık değeri çok yüksek, acilen tıbbi yardım alın.")
