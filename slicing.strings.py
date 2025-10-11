7a1a63bab49ea3a7c38516d197bc2eabc9b7490d
Ahsen Hira Mimtaş <xahsenhiraw@gmail.com>
2025-10-11 14:16:47 +0300

    added slicing strings

---
+++ slicing.strings.py
@@ -0,0 +1,10 @@
+Kelime = "Merhaba"
+
+ilk_ucharf = Kelime[0:3] # 0. indeksten başlayarak 3. indekse kadar (3. indeks dahil değil)
+print(ilk_ucharf) # Mer
+son_ucharf = Kelime[-3:] # sondan 3 harfi alır
+print(son_ucharf) # aba 
+kelime_ters = Kelime[::-1] # baştan sona kadar -1 er atlayarak alır yani ters çevirir
+print(kelime_ters) # abahreM
+
+### w3schools web sitesinden örneklere bakabiirsin.
