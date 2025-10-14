d8c0b42c53e45a36029ccc93fc578b701422e64e
Ahsen Hira Mimtaş <xahsenhiraw@gmail.com>
2025-10-14 21:40:57 +0300

    .

--- odev4.py
+++ odev4.py
@@ -1,1 +1,14 @@
-print(len(input("İsminizi giriniz: ")))
+# for döngüsü ödev
+
+baslangic = int(input("Başlangıç sayısını giriniz: "))
+bitis = int(input("Bitiş sayısını giriniz: "))  
+
+toplam = 0
+print("Tek sayılar:")
+
+for sayi in range(baslangic, bitis + 1):
+    if sayi % 2 !=0:
+        print(sayi)
+        toplam += sayi
+
+print( "Tek sayıların toplamı:", toplam)
